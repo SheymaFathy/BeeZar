4a4f6ea2f8e9bc186e25e738e90d9a704ee865bd
SheymaFathy <freelance.ss22@gmail.com>
2023-08-09 02:55:41 +0300

    00 commit

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+Color mainColor = const Color(0xff5e17eb);
+Color itemColor = const Color(0xffc86ea0);
+Color? textColor = Colors.grey[600];
+Color? subTitle = Colors.grey[500];
+Color titleColor = Colors.black;
+Color backGroungColor = Color(0xff858585);
+Color btnBglight = Color(0xffd3d1d1);
